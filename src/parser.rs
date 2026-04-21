92ccd5566846b50ba25790c0c1ada41ed20c4b61
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2026-04-21 21:55:32 +0200

    Feat: Hexadecimal literals up to 65536

--- src/parser.rs
+++ src/parser.rs
@@ -3,9 +3,12 @@ use std::ops::Range;
 use winnow::prelude::*;
 
 use winnow::combinator::alt;
+use winnow::combinator::cut_err;
+use winnow::combinator::peek;
 use winnow::combinator::repeat;
 //use winnow::combinator::seq;
 use winnow::Result;
+use winnow::error::ContextError;
 use winnow::error::StrContext;
 use winnow::error::StrContextValue;
 use winnow::stream::LocatingSlice;
@@ -62,6 +65,44 @@ fn word<'s>(input: &mut In<'s>) -> Result<(&'s str, Range<usize>)> {
         take_while(0.., |c: char| c.is_alphanum() || c == '_')
     )
     .take()
+    .with_span()
+    .parse_next(input)
+}
+
+const HEX_CHARS: [char; 16 + 6] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+fn underscored<'s>(char_class: &'static [char]) -> impl ModalParser<In<'s>, (), ContextError> {
+    (
+        '_',
+        // If the character after the underscore is not a hexadecimal digit, parsing fails here.
+        cut_err(peek(one_of(char_class))),
+    )
+    .take()
+    .value(())
+}
+
+fn number_hex(input: &mut In) -> ModalResult<(u16, Range<usize>)> {
+    // Assumption: We already *know* and expect that what follows *must* be a hexadecimal number.
+    // GRAMMAR: HEXDIGIT -> '0'..'9' | 'a'..'f' | 'A'..'F'
+    // GRAMMAR: number_hex -> HEXDIGIT ( HEXDIGIT | ( '_' &HEXDIGIT ) )*
+    // GRAMMAR:   # i.e. underscore is only permitted at most once in a row, and cannot be at either start or end.
+    peek(one_of(HEX_CHARS))
+        .context(StrContext::Label("hexadecimal number"))
+        .context(StrContext::Expected(StrContextValue::Description("hexadecimal digit (i.e. 0-9, a-f, A-F)")))
+        .parse_next(input)
+        ?;
+    repeat::<_, _, String, _, _>(
+        1..,
+        alt((
+            one_of(HEX_CHARS).take(),
+            underscored(&HEX_CHARS).value(""),
+        )),
+    )
+    .verify(|s: &String| s.len() <= 4)
+    .try_map(|s: String| u16::from_str_radix(&s, 16))
+    .context(StrContext::Label("hexadecimal number with at most 4 hexits"))
+    .context(StrContext::Expected(StrContextValue::Description("hexit (i.e. 0-9, a-f, A-F)")))
+    .context(StrContext::Expected(StrContextValue::Description("underscore (followed by a hexit)")))
     .with_span()
     .parse_next(input)
 }
@@ -263,6 +304,112 @@ mod tests {
         let output = word(&mut input).expect("parse failed");
         assert_eq!(output, ("world", 6..11));
         assert_eq!(*input, "");
+    }
+
+    #[test]
+    fn test_numhex_minimal_digit() {
+        let mut input = In::new("9yooo");
+        let output = number_hex(&mut input).expect("parse failed");
+        assert_eq!(output, (9, 0..1));
+        assert_eq!(*input, "yooo");
+    }
+
+    #[test]
+    fn test_numhex_minimal_hexit() {
+        let mut input = In::new("ayooo");
+        let output = number_hex(&mut input).expect("parse failed");
+        assert_eq!(output, (10, 0..1));
+        assert_eq!(*input, "yooo");
+    }
+
+    #[test]
+    fn test_numhex_max() {
+        let mut input = In::new("ffffun!");
+        let output = number_hex(&mut input).expect("parse failed");
+        assert_eq!(output, (65535, 0..4));
+        assert_eq!(*input, "un!");
+    }
+
+    #[test]
+    fn test_numhex_several_zero() {
+        let mut input = In::new("00");
+        let output = number_hex(&mut input).expect("parse failed");
+        assert_eq!(output, (0, 0..2));
+        assert_eq!(*input, "");
+    }
+
+    #[test]
+    fn test_numhex_too_long() {
+        let input = In::new("12345");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 0..1);
+        let expected_err = "12345\n^\ninvalid hexadecimal number with at most 4 hexits\nexpected hexit (i.e. 0-9, a-f, A-F), underscore (followed by a hexit)";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_too_long_zero() {
+        let input = In::new("00000");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 0..1);
+        let expected_err = "00000\n^\ninvalid hexadecimal number with at most 4 hexits\nexpected hexit (i.e. 0-9, a-f, A-F), underscore (followed by a hexit)";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_empty() {
+        let input = In::new("");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 0..0);
+        let expected_err = "\n^\ninvalid hexadecimal number\nexpected hexadecimal digit (i.e. 0-9, a-f, A-F)";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_invalid() {
+        let input = In::new("g");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 0..1);
+        let expected_err = "g\n^\ninvalid hexadecimal number\nexpected hexadecimal digit (i.e. 0-9, a-f, A-F)";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_invalid_tail() {
+        let input = In::new("efgh");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 2..3);
+        // Message is only bad because it matches against eof() under the hood, so can't inject anything
+        let expected_err = "efgh\n  ^\n";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_max_underscores() {
+        let mut input = In::new("f_a_c_e+");
+        let output = number_hex(&mut input).expect("parse failed");
+        assert_eq!(output, (0xFACE, 0..7));
+        assert_eq!(*input, "+");
+    }
+
+    #[test]
+    fn test_numhex_double_underscore() {
+        let input = In::new("12__3 * 9");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 3..4);
+        // Message is only bad because it matches against eof() under the hood, so can't inject anything
+        let expected_err = "12__3 * 9\n   ^\ninvalid hexadecimal number with at most 4 hexits\nexpected hexit (i.e. 0-9, a-f, A-F), underscore (followed by a hexit)";
+        assert_eq!(actual_err.to_string(), expected_err);
+    }
+
+    #[test]
+    fn test_numhex_leading_underscore() {
+        let input = In::new("_eee");
+        let actual_err = number_hex.parse(input).expect_err("parse succeeded?!");
+        assert_eq!(actual_err.char_span(), 0..1);
+        // Message is only bad because it matches against eof() under the hood, so can't inject anything
+        let expected_err = "_eee\n^\ninvalid hexadecimal number\nexpected hexadecimal digit (i.e. 0-9, a-f, A-F)";
+        assert_eq!(actual_err.to_string(), expected_err);
     }
 
 }
